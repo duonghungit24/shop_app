2de6e9639e9fdcda50fb1030b1c80390825443f0
duonghungit24 <hungduonng721@gmail.com>
2021-07-17 20:36:07 +0700

    update menu and shop

--- src/components/Main/shop/Shop.js
+++ src/components/Main/shop/Shop.js
@@ -1,27 +1,40 @@
 import React, { Component } from "react";
-import { StyleSheet, View, Text, Button ,Dimensions, TouchableOpacity} from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  Dimensions,
+  TouchableOpacity,
+} from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Icon from 'react-native-vector-icons/FontAwesome5';
+import Icon from "react-native-vector-icons/FontAwesome5";
 import Cart from "./cart/Cart";
 import Contact from "./contact/Contact";
 import Home from "./Home/Home";
 import Search from "./search/Search";
 
-const  height  = Dimensions.get('window').height;
+const height = Dimensions.get("window").height;
 
 const Tab = createBottomTabNavigator();
 
 export default function Shop({ navigation }) {
   return (
     <View style={{ flex: 1 }}>
-      <View style={{ justifyContent:'center', height: height/8, backgroundColor:'yellow' }}>
-        <TouchableOpacity style = {{marginTop: 20, marginLeft:15}}
-          onPress = {() => navigation.openDrawer()}
+      <View
+        style={{
+          justifyContent: "center",
+          height: height / 8,
+          backgroundColor: "yellow",
+        }}
+      >
+        <TouchableOpacity
+          style={{ marginTop: 20, marginLeft: 15 }}
+          onPress={() => navigation.openDrawer()}
         >
-        <Icon name="bars" size={30} color="black" />
+          <Icon name="bars" size={30} color="black" />
         </TouchableOpacity>
-            
       </View>
       <Tab.Navigator>
         <Tab.Screen name="Home" component={Home} />
